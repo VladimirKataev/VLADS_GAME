23f9e5858376e78d12ab90040d4c6ab54f954e67
Vladimir Kataev <vova.kataev@gmail.com>
2020-04-09 13:41:34 -0700

    move method still needs to get faster

--- source/Board.cpp
+++ source/Board.cpp
@@ -137,3 +137,67 @@ std::vector<char> Board::getAllowedMoves() const{
 	}
 	return v;
 }
+
+char Board::changeDir(char pos, char tst){
+	char pC = pos & 0xF;
+	char pR = pos >> 4;
+	char tR = tst >> 4;
+	char tC = tst & 0xF;
+	char cR, cC, chg;
+	if(!board[tst]) return 0;
+	if(tst & 0x88) return 0; // nope if hit on 10001000 mask, good for checking OOB
+	if(pos == tst) return 0; //obvs
+	if((pC - tC != 1 && tC - pC != 1 && pC != tC) ||
+		(pR - tR != 1 && tR - pR != 1 && pR != tR)) return 0; //return 0 if the 2 tested aren't neighbours
+
+	bool 	up = (tR < pR),
+				down = (tR > pR),
+				left = (tC < pC),
+				right = (tC > pC);
+	//std::cout << "pR:" << (int)pR << " pC:" << (int)pC << " tC:" << (int)tC << " tR:" << (int)tR << '\n';
+
+	char ans = 0;
+	char combo = 0;
+	char move = oMove()?1:3;
+	//std::cout << "UDLR:" << up << down << left << right <<"\n";
+	while((tR < 8 && !(tR & 0x80)) && (tC < 8 && !(tC & 0x80)) && getSquare(tR, tC)){
+		if(getSquare(tR, tC) == move){
+			cR = tR;
+			cC = tC;
+			for(char d = 1; d <= combo; d++){
+				if(up){cR = tR + d;}
+				if(down){cR = tR - d;}
+				if(left){cC = tC + d;}
+				if(right){cC = tC - d;}
+				chg = rcToChar(cR, cC);
+				board[chg] = move;
+			}
+		}
+		else
+			combo++;
+		if(up) tR--;
+		if(down) tR++;
+		if(left) tC--;
+		if(right) tC++;
+	}
+	//std::cout << "testDir done for "<< ans <<"\n";
+	return ans;
+}
+char Board::move(char pos){
+	if(!allowedMove(pos)) return 0; //will be optimised out when game class is made
+	char r = pos >> 4;
+	char c = pos & 0xF;
+	char ans =
+	changeDir(pos, rcToChar(r + 1, c))+
+	changeDir(pos, rcToChar(r - 1, c))+
+	changeDir(pos, rcToChar(r + 1, c+1))+
+	changeDir(pos, rcToChar(r + 1, c-1))+
+	changeDir(pos, rcToChar(r - 1, c+1))+
+	changeDir(pos, rcToChar(r - 1, c-1))+
+	changeDir(pos, rcToChar(r, c - 1))+
+	changeDir(pos, rcToChar(r, c+1));
+	board[pos] = xMove()?3:1;
+	moves++;
+	return ans;
+
+}
--- source/Board.h
+++ source/Board.h
@@ -26,6 +26,8 @@ public:
 
 	char move(char pos); //the char return is how many are converted
 	char testDir(char mov, char tst) const;
+	char changeDir(char pos, char tst);
+
 	char allowedMove(char pos) const; //see above
 	char getMoves() const;//How many moves are done
 	bool xMove() const;
